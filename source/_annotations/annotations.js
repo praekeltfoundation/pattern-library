97489b694491b9dd8127db44c06a1f18ec0ca938
Mitso <mqalaba@gmail.com>
2016-11-09 17:47:01 +0200

    pattern-lab initial cleaned up project - remove automated dirs

---
+++ source/_annotations/annotations.js
@@ -0,0 +1,16 @@
+{
+  "comments" : [
+    {
+      "el": "#annotation-css-selector",
+      "title" : "Annotation title",
+      "comment": "Annotation description"
+    }
+  ],
+  "breadcrumbs" : [
+    {
+      "el" : ".breadcrumbs",
+      "title" : "When to use breadcrumbs",
+      "comment" : "Breadcrumbs are used withing an article to serve as directional navigation"
+    }
+  ]
+}
